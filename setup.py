abc1ee276e1231fe41e394c7dc35d20b0a0eb776
Armin Ronacher <armin.ronacher@active-4.com>
2011-06-22 11:24:13 +0200

    Moved code from my sandbox repository

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='pretty',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='0.1',
+    url='http://dev.pocoo.org/hg/sandbox',
+    py_modules=['pretty'],
+    description='extensible pprint successor.',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
